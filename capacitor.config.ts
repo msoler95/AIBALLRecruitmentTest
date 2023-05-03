c11aa6f39556f6bf067b6e632fa45cfedc13b42e
Marc <msoler955@gmail.com>
2023-05-03 15:36:33 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'AirballRecruitmentTest',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
